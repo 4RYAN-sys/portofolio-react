48978848796896316c6513d976ab49e161c1ef14
4RYAN-sys <susantoaryan488@gmail.com>
2025-08-18 15:13:02 +0700

    dah

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 import "../styles/Skills.css";
 import { FaReact, FaHtml5, FaCss3Alt, FaGitAlt, FaJava } from "react-icons/fa";
 import { SiJavascript, SiVite, SiFigma, SiCanva, SiVercel, SiGithub, SiGitlab, SiNetlify } from "react-icons/si";
-import { SiCsharp } from "react-icons/si";
+import { SiDotnet} from "react-icons/si";
 
 export default function Skills() {
   return (
@@ -15,7 +15,7 @@ export default function Skills() {
         <div className="skill-item"><FaReact color="#61dafb" size={50}/> <p>React</p></div>
         <div className="skill-item"><SiVite color="#646cff" size={50}/> <p>Vite</p></div>
         <div className="skill-item"><FaGitAlt color="#f34f29" size={50}/> <p>Git</p></div>
-        <div className="skill-item"><SiCsharp color="#9b4f96" size={50}/> <p>C#</p></div>
+        <div className="skill-item"><SiDotnet color="#9b4f96" size={50}/> <p>C#</p></div>
         <div className="skill-item"><FaJava color="#f89820" size={50}/> <p>Java</p></div>
         <div className="skill-item"><SiCanva color="#00c4cc" size={50}/> <p>Canva</p></div>
         <div className="skill-item"><SiFigma color="#f24e1e" size={50}/> <p>Figma</p></div>
